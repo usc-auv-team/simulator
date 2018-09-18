4d883d605fd35e10a5fd544de543f80467e7d60e
AustianoSC <arlamb@usc.edu>
2018-09-17 21:31:46 -0700

    Added code to publish keyboard input from unity.

--- Assets/Scripts/AUVController.cs
+++ Assets/Scripts/AUVController.cs
@@ -34,48 +34,46 @@ public class AUVController : MonoBehaviour {
         rosSocket.Publish(publicationId, Message);
     }
 
-	void FixedUpdate () {
-		// 3D controll implementation
-		float moveX=0,moveY=0,moveZ=0;
+    void FixedUpdate()
+    {
+        //Basic Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            //move  up
+            Message.data = "W";
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            //move  left
+            Message.data = "A";
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            //move  down
+            Message.data = "S";
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //move  right
+            Message.data = "D";
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            //move  down
+            Message.data = "LShift";
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            //move  up
+            Message.data = "Space";
+        }
 
-		//Basic Movement
-		if (Input.GetKey(KeyCode.W)){
-			//move  up
-			moveZ=1;
-		}
-		if (Input.GetKey(KeyCode.A)){
-			//move  left
-			moveX=-1;
-		}
-		if (Input.GetKey(KeyCode.S)){
-			//move  down
-			moveZ=-1;
-		}
-		if (Input.GetKey(KeyCode.D)){
-			//move  right
-			moveX=1;
-		}
-		if (Input.GetKey(KeyCode.LeftShift)){
-			//move  down
-			moveY=-1;
-		}
-		if (Input.GetKey(KeyCode.Space)){
-			//move  up
-			moveY=1;
-		}
+        // Publish the message
+        rosSocket.Publish(publicationId, Message);
+    }
 
-		Vector3 movement = new Vector3 (moveX, moveY, moveZ);
-
-		// I choosed force control instead of direct transform control for the AUV because
-		// it seems more realistic to apply force to the sub.
-		rb.AddForce (movement * speed);
-
-		// TODO: set differnt mode of controlling the sub including the at least one for 
-		// convenience and one for more realistic test 
-	}
-
-	// Updates the position and velocity information of our AUV
-	void updatePositionText() {
+    // Updates the position and velocity information of our AUV
+    void updatePositionText() {
 		position.text = "AUV position:" +
 		"\nx: " + gameObject.transform.position.x.ToString () +
 		"\ny: " + gameObject.transform.position.y.ToString () +
