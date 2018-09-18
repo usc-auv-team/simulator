e8daa767a2eca87dd86d44b29854c663e1479e31
Swapnil Patel <swapnil@usc.edu>
2018-09-17 20:07:35 -0700

    Added some ROS connection
    Added base code for the connection.

--- Assets/Scripts/AUVController.cs
+++ Assets/Scripts/AUVController.cs
@@ -1,30 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using RosSharp.RosBridgeClient;
 
+[RequireComponent(typeof(RosConnector))]
 public class AUVController : MonoBehaviour {
 
 	public Text position;
 
 	Rigidbody rb;
 	float speed;
+    //static readonly string uri = "ws://192.168.70.129:9090";
+    private RosSocket rosSocket;
+    public string topic = "/message";
+    private StandardString Message;
+    private string publicationId;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
 		speed = 5.0f;
 		rb = GetComponent<Rigidbody> ();
 		Debug.Log (GetComponent<MeshFilter>().mesh.bounds);
-	}
+        rosSocket = GetComponent<RosConnector>().RosSocket;
+        publicationId = rosSocket.Advertise(topic, "std_msgs/String");
+        Message = new StandardString();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		updatePositionText ();
-	}
+        Vector3 values = new Vector3(Random.Range(0, 1000.0f), Random.Range(0, 1000.0f), Random.Range(0, 1000.0f));
+        Message.data = values.ToString();
+        rosSocket.Publish(publicationId, Message);
+    }
 
 	void FixedUpdate () {
 		// 3D controll implementation
 		float moveX=0,moveY=0,moveZ=0;
+
+		//Basic Movement
 		if (Input.GetKey(KeyCode.W)){
 			//move  up
 			moveZ=1;
