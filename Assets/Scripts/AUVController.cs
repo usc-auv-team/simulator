536e6b9ab0ecf47a26488b1cc29e5e7c84d844d3
Swap <swapnil@usc.edu>
2018-09-17 21:50:39 -0700

    Fixed ROS communication

--- Assets/Scripts/AUVController.cs
+++ Assets/Scripts/AUVController.cs
@@ -2,85 +2,105 @@ using UnityEngine;
 using UnityEngine.UI;
 using RosSharp.RosBridgeClient;
 
-[RequireComponent(typeof(RosConnector))]
 public class AUVController : MonoBehaviour {
 
-	public Text position;
+    public Text position;
 
-	Rigidbody rb;
-	float speed;
+    Rigidbody rb;
+    float speed;
     //static readonly string uri = "ws://192.168.70.129:9090";
     private RosSocket rosSocket;
-    public string topic = "/message";
     private StandardString Message;
     private string publicationId;
+    private string subscriptionId;
 
 
     // Use this for initialization
-    void Start () {
-		speed = 5.0f;
-		rb = GetComponent<Rigidbody> ();
-		Debug.Log (GetComponent<MeshFilter>().mesh.bounds);
-        rosSocket = GetComponent<RosConnector>().RosSocket;
-        publicationId = rosSocket.Advertise(topic, "std_msgs/String");
+    void Start() {
+        speed = 5.0f;
+        rb = GetComponent<Rigidbody>();
+        Debug.Log(GetComponent<MeshFilter>().mesh.bounds);
+        rosSocket = new RosSocket("ws://192.168.56.101:9090");
+        publicationId = rosSocket.Advertise("/message", "std_msgs/String");
+        //subscriptionId = rosSocket.Subscribe("/listener", "std_msgs/String", subscriptionHandler);
         Message = new StandardString();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		updatePositionText ();
-        Vector3 values = new Vector3(Random.Range(0, 1000.0f), Random.Range(0, 1000.0f), Random.Range(0, 1000.0f));
-        Message.data = values.ToString();
-        rosSocket.Publish(publicationId, Message);
+
+    // Update is called once per frame
+    void Update() {
+        updatePositionText();
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
+        // 3D controll implementation
+        //float moveX = 0, moveY = 0, moveZ = 0;
+
         //Basic Movement
         if (Input.GetKey(KeyCode.W))
         {
-            //move  up
-            Message.data = "W";
+            //move  forward
+            //moveZ = 1;
+            Message.data = "FORWARD";
         }
-        else if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             //move  left
-            Message.data = "A";
+            //moveX = -1;
+            Message.data = "LEFT";
         }
-        else if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
-            //move  down
-            Message.data = "S";
+            //move  back
+            //moveZ = -1;
+            Message.data = "BACKWARD";
         }
-        else if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             //move  right
-            Message.data = "D";
+            //moveX = 1;
+            Message.data = "RIGHT";
         }
-        else if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             //move  down
-            Message.data = "LShift";
+            //moveY = -1;
+            Message.data = "DOWN";
         }
-        else if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             //move  up
-            Message.data = "Space";
+            //moveY = 1;
+            Message.data = "UP";
         }
 
-        // Publish the message
         rosSocket.Publish(publicationId, Message);
+
+
+        //Vector3 movement = new Vector3 (moveX, moveY, moveZ);
+
+        // I choosed force control instead of direct transform control for the AUV because
+        // it seems more realistic to apply force to the sub.
+        //rb.AddForce (movement * speed);
+
+        // TODO: set differnt mode of controlling the sub including the at least one for 
+        // convenience and one for more realistic test 
     }
 
     // Updates the position and velocity information of our AUV
     void updatePositionText() {
-		position.text = "AUV position:" +
-		"\nx: " + gameObject.transform.position.x.ToString () +
-		"\ny: " + gameObject.transform.position.y.ToString () +
-		"\nz: " + gameObject.transform.position.z.ToString () +
-		"\nAUV velocity:" +
-		"\nx: " + rb.velocity.x.ToString () +
-		"\ny: " + rb.velocity.y.ToString () +
-		"\nz: " + rb.velocity.z.ToString ();
-	}
+        position.text = "AUV position:" +
+        "\nx: " + gameObject.transform.position.x.ToString() +
+        "\ny: " + gameObject.transform.position.y.ToString() +
+        "\nz: " + gameObject.transform.position.z.ToString() +
+        "\nAUV velocity:" +
+        "\nx: " + rb.velocity.x.ToString() +
+        "\ny: " + rb.velocity.y.ToString() +
+        "\nz: " + rb.velocity.z.ToString();
+    }
+
+    void subscriptionHandler(Message message) {
+        StandardString standardString = (StandardString)message;
+        Debug.Log(standardString.data);
+    }
+
 }
