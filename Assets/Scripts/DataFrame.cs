0a8959dc451110c7f2ce500b4674941607395753
Jamie Chen <jjchen@usc.edu>
2018-10-15 21:27:44 -0700

    Added DataFrame object

---
+++ Assets/Scripts/DataFrame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataFrame
+{
+    public int timeStamp;
+
+    public List<BasicObject> objects; //data of external objectss
+
+    //vehicle data
+    //position
+    public Vector3 position;
+    //acceleration
+    public Vector3 acceleration;
+    //velocity
+    public Vector3 speed;
+    //depth
+    public double depth;
+    //rotation
+    public double rotation;
+}
