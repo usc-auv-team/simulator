db95be8248c15ecbc4e5aa9ce2b6a474523b13df
Gabriel Lacayo <glacayo@usc.edu>
2019-07-08 19:50:13 -0700

    Add TestConnection button with temp funcionality and status indicator

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,19 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsMenu : MonoBehaviour
-{
+public class SettingsMenu : MonoBehaviour {
 
     private bool isActive = false;
 
-    public void Start()
-    {
+    public void Start() {
         // By default, this game object should be disabled
         gameObject.SetActive(isActive);
     }
 
-    public void ToggleActive()
-    {
+    public void ToggleActive() {
         isActive = !isActive;
         gameObject.SetActive(isActive);
     }
