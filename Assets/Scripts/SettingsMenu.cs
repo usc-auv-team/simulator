e6dd7187279fd8dac19d364d5b5c101ccf5ef45a
Gabriel Lacayo <glacayo@usc.edu>
2019-07-08 18:30:13 -0700

    Add Settings Menu prototype and button

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    private bool isActive = false;
+
+    public void Start()
+    {
+        // By default, this game object should be disabled
+        gameObject.SetActive(isActive);
+    }
+
+    public void ToggleActive()
+    {
+        isActive = !isActive;
+        gameObject.SetActive(isActive);
+    }
+}
