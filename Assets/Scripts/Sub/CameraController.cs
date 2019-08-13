a5bced8386f7a39e499cda1baceb36ac16cc9491
glacay30 <glacayo@usc.edu>
2019-08-13 13:45:40 -0700

    Add CameraController; Update Sub Prefab
    Created script CameraController and moved CameraMovement into
    it.
    
    Removed CameraMovement and CameraAvoidance from Sub Prefab
    for the time being while I combine them into CameraController.
    
    Reasoning:
    Instead of two separate scripts that deal with controlling the
    camera, it would be easier if they were to merge into a single
    script that is able to control both purposes without having two
    separate systems of manipulating the camera.
    
    This would of course mean a larger class rather than two subclasses,
    but at least until both features are fully working and without
    stepping on each other, using a single class is the temp answer.

--- Assets/Scripts/Sub/CameraController.cs
+++ Assets/Scripts/Sub/CameraController.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour {
+public class CameraController : MonoBehaviour {
 
-    [SerializeField] private Transform cameraTarget = null;
+    [SerializeField] private GameObject cam = null;
+
     [SerializeField] private float xSpeed = 100f;
     [SerializeField] private float ySpeed = 100.0f;
-    [SerializeField] private float yMinLimit = -90f;
-    [SerializeField] private float yMaxLimit = 90f;
+    [SerializeField] private float pitchMinLimit = -90f;
+    [SerializeField] private float pitchMaxLimit = 90f;
     [SerializeField] private float smoothTime = 50f;
 
     [SerializeField] private float zoomSpeed = 3.0f;
@@ -17,46 +18,46 @@ public class CameraMovement : MonoBehaviour {
 
     private Vector3 prevPos = Vector3.zero;
 
-    private float rotationYAxis = 0.0f;
-    private float rotationXAxis = 0.0f;
-    private float velocityX = 0.0f;
-    private float velocityY = 0.0f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    private float velocityHorizontal = 0.0f;
+    private float velocityVertical = 0.0f;
 
     private void Start() {
-        rotationYAxis = transform.eulerAngles.y;
-        rotationXAxis = transform.eulerAngles.x;
+        yaw = transform.eulerAngles.y;
+        pitch = transform.eulerAngles.x;
     }
 
     private void LateUpdate() {
 
         // Save camera position before applying transformations
-        prevPos = cameraTarget.position;
+        prevPos = cam.transform.position;
 
         ZoomCamera();
         MoveCamera();
 
         // Check if the updated position is different from the previous position
-        cameraTarget.hasChanged = !(prevPos == cameraTarget.position);
+        cam.transform.hasChanged = !(prevPos == cam.transform.position);
     }
 
     // If right mouse button is held down, rotate the object so the camera follows it
     private void MoveCamera() {
 
         if (Input.GetMouseButton(1)) {
-            velocityX += xSpeed * Input.GetAxis("Mouse X") * 0.02f;
-            velocityY += ySpeed * Input.GetAxis("Mouse Y") * 0.02f;
+            velocityHorizontal += xSpeed * Input.GetAxis("Mouse X") * 0.02f;
+            velocityVertical += ySpeed * Input.GetAxis("Mouse Y") * 0.02f;
         }
 
-        rotationYAxis += velocityX;
-        rotationXAxis -= velocityY;
-        rotationXAxis = ClampAngle(rotationXAxis, yMinLimit, yMaxLimit);
+        yaw += velocityHorizontal;
+        pitch -= velocityVertical;
+        pitch = ClampAngle(pitch, pitchMinLimit, pitchMaxLimit);
         Quaternion fromRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
-        Quaternion toRotation = Quaternion.Euler(rotationXAxis, rotationYAxis, 0);
+        Quaternion toRotation = Quaternion.Euler(pitch, yaw, 0);
         Quaternion rotation = toRotation;
 
         transform.rotation = rotation;
-        velocityX = Mathf.Lerp(velocityX, 0, Time.deltaTime * smoothTime);
-        velocityY = Mathf.Lerp(velocityY, 0, Time.deltaTime * smoothTime);
+        velocityHorizontal = Mathf.Lerp(velocityHorizontal, 0, Time.deltaTime * smoothTime);
+        velocityVertical = Mathf.Lerp(velocityVertical, 0, Time.deltaTime * smoothTime);
     }
 
     // If right mouse button is held down, move the camera closer or farther
@@ -64,7 +65,7 @@ public class CameraMovement : MonoBehaviour {
 
         if (Input.GetMouseButton(1)) {
 
-            Vector3 newCameraPosition = cameraTarget.transform.localPosition;
+            Vector3 newCameraPosition = cam.transform.localPosition;
 
             float scrollDelta = Input.GetAxisRaw("Mouse ScrollWheel") * zoomSpeed;
             newCameraPosition.z += scrollDelta;
@@ -72,7 +73,7 @@ public class CameraMovement : MonoBehaviour {
             if (newCameraPosition.z > zoomMin) { newCameraPosition.z = zoomMin; }
             else if (newCameraPosition.z < zoomMax) { newCameraPosition.z = zoomMax; }
 
-            cameraTarget.transform.localPosition = newCameraPosition;
+            cam.transform.localPosition = newCameraPosition;
         }
     }
 
