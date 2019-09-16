35e2456f22f68756084b11991824b9ad9a072572
glacay30 <glacayo@usc.edu>
2019-09-16 16:02:35 -0700

    Add camera switcher; Rename CameraController
    Pressing the C button will now switch the camera between a third
    and first person perspective, handled by the CameraSwitcher
    component added to the CameraPivot game object.
    
    Changed CameraController to CameraThirdPersonController due
    to the ambiguity caused when the sim now has two camera to handle.
    Because it was in charge of handling the camera, its name was
    changed to be more specific about what camera it's supposed to
    manage.
    
    In addition to switching between the normal, third person camera
    and the first person camera, the script will also disable the
    CameraThirdPersonController should it not be needed.

---
+++ Assets/Scripts/Sub/CameraSwitcher.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour {
+
+
+    // ******************************************************
+    // Relevant Camera Components Class
+
+    private class CameraComponents {
+        public Camera cam = null;
+        public AudioListener audio = null;
+
+        public void SetState(bool enabled) {
+            cam.enabled = enabled;
+            audio.enabled = enabled;
+        }
+
+        public void Toggle() {
+            cam.enabled = !cam.enabled;
+            audio.enabled = !audio.enabled;
+        }
+    }
+
+    // ******************************************************
+    // Camera State Enum
+
+    private enum CameraState { FIRST, THIRD }
+
+    // ******************************************************
+    // Game Object References
+
+    [SerializeField] private GameObject CameraObjectFirst = null;
+    [SerializeField] private GameObject CameraObjectThird = null;
+    private CameraThirdPersonController TPController = null;
+
+    // ******************************************************
+    // Fields
+
+    private CameraComponents camFirst;
+    private CameraComponents camThird;
+    private CameraState state = CameraState.THIRD;
+
+    // ******************************************************
+    // Monobehavior Methods
+
+    private void Start() {
+        SetObjectReferences();
+        camFirst.SetState(true);
+        camThird.SetState(false);
+
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.C)) {
+            ToggleCameraState();
+            ToggleActiveCamera();
+        }
+    }
+
+    // ******************************************************
+    // Private Methods
+
+    private void SetObjectReferences() {
+        if (!CameraObjectFirst) {
+            Debug.LogError("First Person Camera not set!");
+        }
+        if (!CameraObjectThird) {
+            Debug.LogError("Third Person Camera not set!");
+        }
+
+        camFirst = new CameraComponents {
+            cam = CameraObjectFirst.GetComponent<Camera>(),
+            audio = CameraObjectFirst.GetComponent<AudioListener>()
+        };
+
+        camThird = new CameraComponents {
+            cam = CameraObjectThird.GetComponent<Camera>(),
+            audio = CameraObjectThird.GetComponent<AudioListener>()
+        };
+
+        TPController = GetComponent<CameraThirdPersonController>();
+    }
+
+    private void ToggleCameraState() {
+        switch (state) {
+            case CameraState.FIRST:
+                state = CameraState.THIRD;
+                if (TPController) { TPController.enabled = true; }
+                break;
+            case CameraState.THIRD:
+                state = CameraState.FIRST;
+                if (TPController) { TPController.enabled = false; }
+                break;
+        }
+    }
+
+    private void ToggleActiveCamera() {
+        camFirst.Toggle();
+        camThird.Toggle();
+    }
+}
