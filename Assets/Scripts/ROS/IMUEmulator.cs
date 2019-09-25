c50cb0d8ede20f8d4c62fb4f1b660b63cc3a1ed3
Swap <swapnil@usc.edu>
2019-09-24 20:59:13 -0700

    Add initial IMUEmulator class template

---
+++ Assets/Scripts/ROS/IMUEmulator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IMUEmulator : ROSComponent {
+    [SerializeField] private string topic = "/ngimu/euler";
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void FixedUpdate() {
+        if(!IsEnabled) {
+            return;
+        }
+
+        // Publish IMU data
+    }
+}
