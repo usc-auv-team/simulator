aa5f678173106b57d1916c5198fcf36430ffba35
Swap <swapnil@usc.edu>
2019-09-24 20:57:11 -0700

    Add initial ROSComponent class implementation
    This class abstracts any component that interacts with ROS since they
    should all have the ability to be enabled or disabled
    
    This also makes it easier to add ROS component specific features later
    without having to edit every ROS component class

---
+++ Assets/Scripts/ROS/ROSComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class ROSComponent : MonoBehaviour {
+    protected static ROSConnector connector = ROSConnector.Instance;
+    private bool isEnabled = connector.status == ROSConnector.Status.SUCCESS;
+    public bool IsEnabled {
+        get { return isEnabled; }
+        set => isEnabled = value && (connector.status == ROSConnector.Status.SUCCESS);
+    }
+}
+
