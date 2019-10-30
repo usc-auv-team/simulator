571dff1d284e4da1b8ebb9928a19035a35d71b08
Philippe Ollivier <pollivie@usc.edu>
2019-10-29 21:15:46 -0700

    Add GUI for motors
    Added graphical interface to see the status of the motors.

---
+++ Assets/Prefabs/GUISubmarine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GUISubmarine : MonoBehaviour
+{
+    [SerializeField] private Image[] images;
+    [SerializeField] private Sprite[] sprites;
+    [SerializeField] private PhysicsSim physicsSim;
+
+   
+    void Update(){
+        for(int i = 0; i < 6; ++i){
+            int j = (physicsSim.motors[i].motorOn) ? (1) : (0);
+            images[i].sprite = sprites[2*i+j];
+        }
+    }
+}
