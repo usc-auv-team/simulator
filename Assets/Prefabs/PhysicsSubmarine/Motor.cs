e253fbe04a62f2ec21d86915e520c90630cc5cbf
Philippe Ollivier <pollivie@usc.edu>
2019-11-11 20:02:33 -0800

    Add power variable to motor
    Power is a float between -1 and 1, which corresponds to the ROS power
    values.

--- Assets/Prefabs/PhysicsSubmarine/Motor.cs
+++ Assets/Prefabs/PhysicsSubmarine/Motor.cs
@@ -12,20 +12,26 @@ public class Motor// : MonoBehaviour
     private float force;
     [SerializeField]
     public bool motorOn;
+    [SerializeField]
+    public float power; //power varies -1 and 1 like the ros does
     private Vector3 globalCog = Vector3.zero;
 
+   
     public void SetGlobalCog(Vector3 realCog) { globalCog = realCog; }
-    public Vector3 GetForce() { return motorDirection * force * ((motorOn) ? (1) : (0)); }
+    public Vector3 GetForce() { return motorDirection * power * force * ((motorOn) ? (1) : (0)); }
     public void SetForce(float inForce) { force = inForce; }
     public void SetMotorDirection(Vector3 inVec) { motorDirection = inVec; }
     public Vector3 GetMotorDirection() { return motorDirection; }
     public void SetDistCog(Vector3 inVec) { distCog = inVec - globalCog; }
     public Vector3 GetTorque(){ return Vector3.Cross(distCog, GetForce()); }
+    public float GetPower() { return power; }
+    public void SetPower(float inPower) { power = Mathf.Clamp(inPower, -1f, 1f); }
 
     public Motor()
     {
         motorDirection = Vector3.zero;
         force = 0f;
+        power = 0f;
         distCog = Vector3.zero;
         motorOn = true;
     }
@@ -37,5 +43,6 @@ public class Motor// : MonoBehaviour
         distCog = _distCog;
         force = _force;
         motorOn = _motorOn;
+        power = 1f;
     }
 }
