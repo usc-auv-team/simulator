26a351177289ccdf91c2d74b962ae5012af85e9b
glacay30 <glacayo@usc.edu>
2020-01-28 21:03:03 -0800

    Create SettingsFields scriptable object
    Purpose is to hold information about the settings menu, which right
    now is only the ip address and port.
    
    Created a default settings menu asset that specifies what the
    values in the fields should default to on startup.

---
+++ Assets/Scripts/HUD/SettingsMenu/SettingsFields.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Settings/Fields")]
+
+public class SettingsFields : ScriptableObject
+{
+    public string ipAddress = "";
+    public string port = "";
+}
