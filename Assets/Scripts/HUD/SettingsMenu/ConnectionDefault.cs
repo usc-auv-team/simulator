bf257322f04b3ff07e8c82b8c2de2e492fda927d
glacay30 <glacayo@usc.edu>
2020-01-28 21:04:53 -0800

    Create ConnectionDefault script
    It will fill the text components of the Ip Address and Port with
    the default values from the given SettingsFields asset.

---
+++ Assets/Scripts/HUD/SettingsMenu/ConnectionDefault.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectionDefault : MonoBehaviour
+{
+
+    [SerializeField] private SettingsFields defaultSettings = null;
+    [SerializeField] private InputField ipAddress = null;
+    [SerializeField] private InputField port = null;
+
+
+    private void Start()
+    {
+        if (ipAddress) {
+            ipAddress.text = defaultSettings.ipAddress;
+        }
+
+        if (port) {
+            port.text = defaultSettings.port;
+        }
+    }
+}
