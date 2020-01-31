18795c728eb04f5b4352fd7de3556086dc03a8c2
Philippe Ollivier <pollivie@usc.edu>
2020-01-30 20:20:49 -0800

    Add BoundingBox functionality
    BoundingBox script works in O(V) time now, simply drag the
    BoundingBox script onto a game object in scene and it will show
    on camera.

--- Assets/Scripts/ROS/BoundingBox.cs
+++ Assets/Scripts/ROS/BoundingBox.cs
@@ -4,22 +4,65 @@ using UnityEngine;
 
 public class BoundingBox : MonoBehaviour
 {
-    public Camera cam;
+    private Camera cam;
+    private float xMax, xMin, yMax, yMin;
+    private Mesh mesh;
+    private static Texture2D whiteTexture;
+
 
     private void Start() {
         cam = Camera.main;
-    }
+        MeshFilter mf = GetComponent<MeshFilter>();
+        if(mf == null) {
+            mesh = GetComponent<SkinnedMeshRenderer>().sharedMesh;
+        }
+        else {
+            mesh = mf.mesh;
+        }
 
-    // Update is called once per frame
-    private void OnDrawGizmos() {
-        Vector2 bl = new Vector2(10, 10), tr = new Vector2(60, 60);
-        DrawSquare(bl, tr);
+        whiteTexture = new Texture2D(1, 1);
+        whiteTexture.SetPixel(0, 0, Color.white);
+        whiteTexture.Apply();
     }
 
     private void OnGUI() {
-        GUI.BeginGroup(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 100));
-       
-        GUI.EndGroup();
+        CalculateAndDrawBorders();
     }
 
+    private void CalculateAndDrawBorders() {
+        Matrix4x4 localToWorld = transform.localToWorldMatrix;
+        xMax = 0;
+        xMin = int.MaxValue;
+        yMax = 0;
+        yMin = int.MaxValue;
+
+        for (int i = 0; i < mesh.vertexCount; i++) {
+            Vector2 v = cam.WorldToScreenPoint(localToWorld.MultiplyPoint3x4(mesh.vertices[i]));
+            if (v.x < xMin) {
+                xMin = v.x;
+            }
+            else if (v.x > xMax) {
+                xMax = v.x;
+            }
+            if (v.y < yMin) {
+                yMin = v.y;
+            }
+            else if (v.y > yMax) {
+                yMax = v.y;
+            }
+        }
+        xMax = Mathf.Clamp(xMax, 0, Screen.width);
+        xMin = Mathf.Clamp(xMin, 0, Screen.width);
+        yMax = Mathf.Clamp(yMax, 0, Screen.height);
+        yMin = Mathf.Clamp(yMin, 0, Screen.height);
+
+        DrawScreenRect(new Rect((int)xMin, (int)(Screen.height - yMin), 
+        (int)(xMax - xMin), (int)(-yMax + yMin)), new Color(0, 0, 1, 0.2f));
+    }
+
+    public static void DrawScreenRect(Rect rect, Color color) {
+        GUI.color = color;
+        GUI.DrawTexture(rect, whiteTexture);
+        GUI.color = Color.white;
+    }
 }
