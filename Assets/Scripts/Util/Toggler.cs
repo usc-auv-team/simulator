68dc3a3637f7fcf3b40c0b1871c811eb3243515a
glacay30 <glacayo@usc.edu>
2020-01-30 20:44:58 -0800

    Create Toggler script
    Util script that offers a method to toggle the active state of
    the game object it's attached to. Created because
    MonoBehvaior.SetActive() can only set with some value, but not
    be able to toggle it. Buttons such as the SettingsMenu toggles
    the UI element, so this script is exactly for that.

---
+++ Assets/Scripts/Util/Toggler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Toggler : MonoBehaviour {
+
+    [SerializeField] bool defaultActiveState;
+    private bool isActive;
+
+    public void Start() {
+        // By default, this game object should be disabled
+        gameObject.SetActive(defaultActiveState);
+        isActive = defaultActiveState;
+    }
+
+    public void ToggleActive() {
+        isActive = !isActive;
+        gameObject.SetActive(isActive);
+    }
+}
